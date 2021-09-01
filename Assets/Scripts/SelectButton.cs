c0516c183015e27ce1bc23a975a3b01c2a452530
AyanoE <75414651+AyanoE@users.noreply.github.com>
2021-09-01 17:21:02 +0900

    CSV読み込み
    CSVを配列にロード
    仮のスタート画面作成
    日付表示をするスペースを作成

---
+++ Assets/Scripts/SelectButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SelectButton : MonoBehaviour
+{
+    // 選択画面に遷移
+    public void SelectButtonDown()
+    {
+        SceneManager.LoadScene("SelectScene");
+    }
+}
