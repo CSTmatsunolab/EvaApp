753cc53d436ddcd88a95d80aac0ec10464bd3321
Shin <cssn18011@g.nihon-u.ac.jp>
2021-09-02 12:39:54 +0900

    水の所持数、水分カウンターの表示

---
+++ Assets/WaterCount.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+public class WaterCount : MonoBehaviour
+{
+    void Start()
+    {
+         GameObject Pdata = GameObject.Find("Player_Data");
+         
+         int x = int.Parse(Pdata.GetComponent<Player_Data>().PlayerData[1][2]);
+         int y = int.Parse(this.name);
+
+         if(y<=x)
+         {
+             this.gameObject.SetActive(true);
+         }
+         else
+         {
+             this.gameObject.SetActive(false);
+         }
+    }
+}
