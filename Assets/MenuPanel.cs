87c9a931aa843eb3c6685f274fd46ea8c656c8e6
Shin <cssn18011@g.nihon-u.ac.jp>
2021-09-16 19:16:38 +0900

    選択画面のデザイン変更
    デザインを大幅に変更しました。

---
+++ Assets/MenuPanel.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+ 
+public class MenuPanel : MonoBehaviour
+{
+    [SerializeField] GameObject Menu;
+    [SerializeField] GameObject Drink;
+
+
+    void Start()
+    {
+        BackToMenu();
+        BackToDrink();
+    }
+
+    public void BackToMenu()
+    {
+        Menu.SetActive(false);
+    }
+
+    public void GoToMenu()
+    {
+        Menu.SetActive(true);
+    }
+
+    public void GoToTitle()
+    {
+        SceneManager.LoadScene("StartScene");
+    }
+
+    public void GoToDrink()
+    {
+        Drink.SetActive(true);
+    }
+
+    public void BackToDrink()
+    {
+        Drink.SetActive(false);
+    }
+
+
+}
---
+++ Assets/TimeBase.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; // 追加
+
+public class TimeBase : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // オブジェクトの取得
+        GameObject image_object = GameObject.Find("TimeBase");
+        GameObject Pdata = GameObject.Find("Player_Data");
+        // コンポーネントの取得
+        Image image_component = image_object.GetComponent<Image>();
+        Texture2D texture0 = Resources.Load("hiru") as Texture2D;
+        Texture2D texture1 = Resources.Load("yoru") as Texture2D;
+        
+        int i = int.Parse(Pdata.GetComponent<Player_Data>().PlayerData[1][6]);
+        if (i == 0)
+            {
+                image_component.sprite = Sprite.Create(texture0,new Rect(0, 0, texture0.width, texture0.height),Vector2.zero);
+            }
+        else if(i == 1)
+            {
+                image_component.sprite = Sprite.Create(texture1,new Rect(0, 0, texture1.width, texture1.height),Vector2.zero);
+            }
+    }
+}
