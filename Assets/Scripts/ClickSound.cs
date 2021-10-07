8d0ca17adfade2b25e50923316ef5683189a7b03
KDIK1217 <csto18029@g.nihon-u.ac.jp>
2021-10-07 18:53:21 +0900

    ボタンの効果音の設定
    選択画面のボタンを押したら効果音が鳴るように設定しました

---
+++ Assets/Scripts/ClickSound.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickSound : MonoBehaviour
+{
+    private AudioSource sound01;
+
+    void Start()
+    {
+        sound01 = GetComponent<AudioSource>();
+    }
+
+    public void OnClick()
+    {
+        sound01.PlayOneShot(sound01.clip);
+    }
+
+}
