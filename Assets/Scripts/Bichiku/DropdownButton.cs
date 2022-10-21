0972f28ffef5bf82364d7a70db1b8eeca1a14fcc
HazeyamaS <csso19084@g.nihon-u.ac.jp>
2022-10-21 18:28:41 +0900

    bichiku 途中

---
+++ Assets/Scripts/Bichiku/DropdownButton.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DropdownButton : MonoBehaviour
+{
+    public Toggle toggle;
+    public GameObject[] ItemBox = new GameObject[17];
+    public GameObject content14;
+    public Scrollbar scroll;
+
+    public void OffToggleChanged(){
+        if(toggle.isOn == false){
+        ItemBox[0] = GameObject.Find("Itembox0");
+        ItemBox[1] = GameObject.Find("Itembox1");
+        ItemBox[2] = GameObject.Find("Itembox2");
+        ItemBox[3] = GameObject.Find("Itembox3");
+        ItemBox[4] = GameObject.Find("Itembox4");
+        ItemBox[5] = GameObject.Find("Itembox5");
+        ItemBox[6] = GameObject.Find("Itembox6");
+        ItemBox[7] = GameObject.Find("Itembox7");
+        ItemBox[8] = GameObject.Find("Itembox8");
+        ItemBox[9] = GameObject.Find("Itembox9");
+        ItemBox[10] = GameObject.Find("Itembox10");
+        ItemBox[11] = GameObject.Find("Itembox11");
+        ItemBox[12] = GameObject.Find("Itembox12");
+        ItemBox[13] = GameObject.Find("Itembox13");
+        ItemBox[14] = GameObject.Find("Itembox14");
+        ItemBox[15] = GameObject.Find("Itembox15");
+        ItemBox[16] = GameObject.Find("Itembox16");
+
+        int i =16;
+        bool flag = true;
+        while(flag == true){
+            print("親の名前は、" + transform.parent.name);
+            print("自分は、" + ItemBox[i].name);
+            if(ItemBox[i].name != transform.parent.name){
+                Vector3 Idou;
+                Idou = ItemBox[i].transform.position;
+                Idou.y = Idou.y - 2.5f;
+                ItemBox[i].transform.position = Idou;
+                print("移動した");
+            }
+            if(ItemBox[i].name == transform.parent.name){
+                flag = false;
+            }
+            i = i - 1;
+        }
+        content14.GetComponent<RectTransform>().sizeDelta = new Vector2 ((content14.GetComponent<RectTransform>().sizeDelta.x),(content14.GetComponent<RectTransform>().sizeDelta.y + 160f));
+        if(transform.parent.name == "Itembox16"){
+            for(int j  = 0 ; j < 5 ; j++){
+                    scroll.value = scroll.value - 1;
+            }
+        }
+        }
+    }
+    public void OnToggleChanged(){
+        if(toggle.isOn){
+        ItemBox[0] = GameObject.Find("Itembox0");
+        ItemBox[1] = GameObject.Find("Itembox1");
+        ItemBox[2] = GameObject.Find("Itembox2");
+        ItemBox[3] = GameObject.Find("Itembox3");
+        ItemBox[4] = GameObject.Find("Itembox4");
+        ItemBox[5] = GameObject.Find("Itembox5");
+        ItemBox[6] = GameObject.Find("Itembox6");
+        ItemBox[7] = GameObject.Find("Itembox7");
+        ItemBox[8] = GameObject.Find("Itembox8");
+        ItemBox[9] = GameObject.Find("Itembox9");
+        ItemBox[10] = GameObject.Find("Itembox10");
+        ItemBox[11] = GameObject.Find("Itembox11");
+        ItemBox[12] = GameObject.Find("Itembox12");
+        ItemBox[13] = GameObject.Find("Itembox13");
+        ItemBox[14] = GameObject.Find("Itembox14");
+        ItemBox[15] = GameObject.Find("Itembox15");
+        ItemBox[16] = GameObject.Find("Itembox16");
+
+        int i =16;
+        bool flag = true;
+        while(flag == true){
+            print("親の名前は、" + transform.parent.name);
+            print("自分は、" + ItemBox[i].name);
+            if(ItemBox[i].name != transform.parent.name){
+                Vector3 Idou;
+                Idou = ItemBox[i].transform.position;
+                Idou.y = Idou.y + 2.5f;
+                ItemBox[i].transform.position = Idou;
+                print("移動した");
+            }   
+            if(ItemBox[i].name == transform.parent.name){
+                flag = false;
+            }
+            i = i - 1;
+        }
+        content14.GetComponent<RectTransform>().sizeDelta = new Vector2 ((content14.GetComponent<RectTransform>().sizeDelta.x),(content14.GetComponent<RectTransform>().sizeDelta.y - 160f));
+        }
+    }
+}
---
+++ Assets/Scripts/Bichiku/check_year.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class check_year : MonoBehaviour
+{
+   public InputField inputField_y;
+   public InputField inputField_m;
+   public InputField inputField_d;
+   public GameObject toggle;
+   DateTime TodayNow;
+   int year;
+   int month;
+   int day;
+   bool y_flag = false;
+   bool m_flag = false;
+   bool d_flag = false;
+
+   // Update is called once per frame
+   public void InputYear()
+   {
+       TodayNow = DateTime.Now;
+       if(inputField_y.text.Length == 4){
+           year = int.Parse(inputField_y.text);
+           y_flag = true;
+       }else{
+           y_flag = false;
+       }
+       Debug.Log("y_flag="+ y_flag);
+       check();
+   }
+
+   public void InputMonth()
+   {
+       TodayNow = DateTime.Now;
+       if(inputField_m.text.Length == 2 || inputField_m.text.Length == 1){
+           month = int.Parse(inputField_m.text);
+           m_flag = true;
+       }else{
+           m_flag = false;
+       }
+       Debug.Log("m_flag="+ m_flag);
+       check();
+   }
+   public void InputDay()
+   {
+       TodayNow = DateTime.Now;
+       if(inputField_d.text.Length == 2 || inputField_d.text.Length == 1){
+           day = int.Parse(inputField_d.text);
+           d_flag = true;
+       }else{
+           d_flag = false;
+       }
+       Debug.Log("d_flag="+ d_flag);
+       check();
+   }
+
+   void check(){
+       if(y_flag == true && m_flag == true && d_flag == true){
+           if(TodayNow.Year == year && TodayNow.Month == month && TodayNow.Day >= day){
+               Debug.Log("本日:"+TodayNow+"以降以降で入力してください");
+               inputField_d.text = null;
+               d_flag = false;
+           }else if(TodayNow.Year == year && TodayNow.Month > month){
+               Debug.Log("本日:"+TodayNow+"以降以降で入力してください");
+               inputField_m.text = null;
+               inputField_d.text = null;
+               m_flag = false;
+               d_flag = false;
+           }else if(TodayNow.Year > year){
+               Debug.Log("本日:"+TodayNow+"以降以降で入力してください");
+               inputField_y.text = null;
+               inputField_m.text = null;
+               inputField_d.text = null;
+               y_flag = false;
+               m_flag = false;
+               d_flag = false;
+           }else{
+               Debug.Log("ok");  
+           }
+       }
+   }
+}
