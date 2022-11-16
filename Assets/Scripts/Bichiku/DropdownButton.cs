a782a5c05e783ed84f0b00134cdb19ec383f284e
HazeyamaS <csso19084@g.nihon-u.ac.jp>
2022-11-16 20:22:27 +0900

    save

--- Assets/Scripts/Bichiku/DropdownButton.cs
+++ Assets/Scripts/Bichiku/DropdownButton.cs
@@ -6,30 +6,14 @@ using UnityEngine.UI;
 public class DropdownButton : MonoBehaviour
 {
     public Toggle toggle;
-    public GameObject[] ItemBox = new GameObject[17];
+    public GameObject[] ItemBox;
+    public GameObject[] test;
     public GameObject content14;
     public Scrollbar scroll;
 
     public void OffToggleChanged(){
         if(toggle.isOn == false){
-        ItemBox[0] = GameObject.Find("Itembox0");
-        ItemBox[1] = GameObject.Find("Itembox1");
-        ItemBox[2] = GameObject.Find("Itembox2");
-        ItemBox[3] = GameObject.Find("Itembox3");
-        ItemBox[4] = GameObject.Find("Itembox4");
-        ItemBox[5] = GameObject.Find("Itembox5");
-        ItemBox[6] = GameObject.Find("Itembox6");
-        ItemBox[7] = GameObject.Find("Itembox7");
-        ItemBox[8] = GameObject.Find("Itembox8");
-        ItemBox[9] = GameObject.Find("Itembox9");
-        ItemBox[10] = GameObject.Find("Itembox10");
-        ItemBox[11] = GameObject.Find("Itembox11");
-        ItemBox[12] = GameObject.Find("Itembox12");
-        ItemBox[13] = GameObject.Find("Itembox13");
-        ItemBox[14] = GameObject.Find("Itembox14");
-        ItemBox[15] = GameObject.Find("Itembox15");
-        ItemBox[16] = GameObject.Find("Itembox16");
-
+        ItemBox = GameObject.FindGameObjectsWithTag("Itembox");
         int i =16;
         bool flag = true;
         while(flag == true){
@@ -57,23 +41,7 @@ public class DropdownButton : MonoBehaviour
     }
     public void OnToggleChanged(){
         if(toggle.isOn){
-        ItemBox[0] = GameObject.Find("Itembox0");
-        ItemBox[1] = GameObject.Find("Itembox1");
-        ItemBox[2] = GameObject.Find("Itembox2");
-        ItemBox[3] = GameObject.Find("Itembox3");
-        ItemBox[4] = GameObject.Find("Itembox4");
-        ItemBox[5] = GameObject.Find("Itembox5");
-        ItemBox[6] = GameObject.Find("Itembox6");
-        ItemBox[7] = GameObject.Find("Itembox7");
-        ItemBox[8] = GameObject.Find("Itembox8");
-        ItemBox[9] = GameObject.Find("Itembox9");
-        ItemBox[10] = GameObject.Find("Itembox10");
-        ItemBox[11] = GameObject.Find("Itembox11");
-        ItemBox[12] = GameObject.Find("Itembox12");
-        ItemBox[13] = GameObject.Find("Itembox13");
-        ItemBox[14] = GameObject.Find("Itembox14");
-        ItemBox[15] = GameObject.Find("Itembox15");
-        ItemBox[16] = GameObject.Find("Itembox16");
+        ItemBox = GameObject.FindGameObjectsWithTag("Itembox");
 
         int i =16;
         bool flag = true;
---
+++ Assets/Scripts/Bichiku/ItemGenerate.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+using UnityEngine.SceneManagement;
+using System.Linq;
+
+public class ItemGenerate : MonoBehaviour
+{
+    int c = 0;
+    public Transform Content;
+    GameObject prefabf;
+    GameObject prefab;
+
+    static List<string[]> itemData = new List<string[]>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(c == 0){
+            generateI();
+        } 
+    }
+
+     void generateI(){
+        prefabf = (GameObject)Resources.Load("Prefabs/Itemboxf");
+        prefab = (GameObject)Resources.Load("Prefabs/Itembox");
+        Vector3 position = new Vector3(0, 0, 0);
+        Instantiate (prefabf,position,Quaternion.identity,Content);
+        
+        itemData = File.ReadAllLines(@"Assets/Resources/Texts/Item_list.csv").Select(line => line.Split(',')).ToList();
+         
+        c = 1;
+     }
+}
