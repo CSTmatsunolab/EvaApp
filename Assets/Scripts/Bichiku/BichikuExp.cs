ffcbd6cfafbfb3b29ba874703e61f076f62ea8f5
HazeyamaS <csso19084@g.nihon-u.ac.jp>
2023-05-09 15:40:39 +0900

    bichikuのexp

---
+++ Assets/Scripts/Bichiku/BichikuExp.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BichikuExp : MonoBehaviour
+{
+    [SerializeField] GameObject Explanation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CloseExplanation();
+        GoToExplanation();   
+    }
+
+    public void GoToExplanation(){//説明パネル表示
+        Explanation.SetActive(true);
+    }
+
+    // Update is called once per frame
+    public void CloseExplanation(){//説明パネル非表示
+        Explanation.SetActive(false);
+    }
+}
