65258cbbb2116dff086f1c9e5d07edcb095adebd
HazeyamaS <csso19084@g.nihon-u.ac.jp>
2024-08-05 15:39:22 +0900

    41~49非表示

--- Assets/Scripts/EventSelect/ESManagement.cs
+++ Assets/Scripts/EventSelect/ESManagement.cs
@@ -89,7 +89,7 @@ public class ESManagement : MonoBehaviour
         if(flag == 0)
         {
 
-            a = Random.Range(3,50);//3~40
+            a = Random.Range(3,41);//3~40
             //a = 56;
             //a = Random.Range(21,23);//指定範囲の動作確認用
             Debug.Log(a);
@@ -102,7 +102,7 @@ public class ESManagement : MonoBehaviour
             {
                 if(kaburiCheck == 1)
                 {
-                    a = Random.Range(3,50);//3~40
+                    a = Random.Range(3,41);//3~40
                     //a = Random.Range(21,23);//指定範囲の動作確認用
                     //a = 50;
                     kaburiCheck = int.Parse(Rdata.GetComponent<Result_Data>().ResultData[a][1]);
@@ -117,7 +117,7 @@ public class ESManagement : MonoBehaviour
             if(b < 5) {
                 a = Random.Range(50,60);
             } else{
-                a = Random.Range(3,50);
+                a = Random.Range(3,41);
             }
 
             kaburiCheck = int.Parse(Rdata.GetComponent<Result_Data>().ResultData[a][1]);
@@ -138,7 +138,7 @@ public class ESManagement : MonoBehaviour
             {
                 if(kaburiCheck == 1)
                 {
-                    a = Random.Range(3,50);//3~40
+                    a = Random.Range(3,41);//3~40
                     kaburiCheck = int.Parse(Rdata.GetComponent<Result_Data>().ResultData[a][1]);
                     Debug.Log("a1 = " + a);
                 }
