58998d6e731e9ebac237cbea84d5b867368f1be1
HazeyamaS <csso19084@g.nihon-u.ac.jp>
2024-12-27 14:06:47 +0900

    プライバシーポリシー
    プライバシーポリシーボタンを追加

---
+++ Assets/Scripts/Title/URL.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class URL : MonoBehaviour
+{
+   public void onClick()
+    {
+        Application.OpenURL("https://www.matsulab.org/#/");//""の中には開きたいWebページのURLを入力します
+    }
+}
